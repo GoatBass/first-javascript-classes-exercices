9e1826eb37c3e579b799a84cfdf1789b4281318b
Edu <edugacv@gmail.com>
2021-01-15 09:56:55 +0100

    Exercice finished

--- ex7.js
+++ ex7.js
@@ -1,16 +1,34 @@
 class AddressBook {
     // Implement the constructor method
+    constructor(){
+        this.elementsName = []
+        this.elementsMail = []
+    }
     // Implement the store method
+    store(name, email){
+        this.elementsName.push(name)
+        this.elementsMail.push(email)
+    }
     // Implement the lookup method
+    lookup(name){
+        for(let i = 0; i < this.elementsMail.length; i++){
+            if(this.elementsMail[i].substring(0, this.elementsMail[i].lastIndexOf('@')) === name){
+                return this.elementsMail[i]
+            }
+        }
+        return 'Adress not found'
+    }
   }
   
-  const myAddresBook = new AddressBook();
+  const myAddressBook = new AddressBook();
   
   myAddressBook.store("bart", "bart@simpsons.com");
   myAddressBook.store("maggie", "maggie@simpsons.com");
   
-  myAddressBook.lookup("bart");
+  console.log(myAddressBook.lookup("bart"));
   // 'bart@simpsons.com'
   
-  myAddressBook.lookup("homer");
+  console.log(myAddressBook.lookup("homer"));
   // 'address not found'
+
+  console.log(myAddressBook.lookup("maggie"));
